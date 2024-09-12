bd6cb784b2645623e36158fd7ff94c6e153579e6
wolffcolin <cwcnsa@gmail.com>
2024-09-12 14:50:55 -0600

    iteration 2

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def main():
     start = "" 
     end = ""
 
+    pseudoStart = ('x', 'y', '', '')
+    pseudoEnd = ('x', 'y', '', '')
+
+    startEstablished = False
+    endEstablished = False
+
     counter = 0
 
-    G = nx.Graph()
+    G = nx.DiGraph()
 
     vertex_dict = {}
-    vertex_dictEnd = {}
-    keyring = set()
-    keyringEnd = set()
 
     with open("maze.txt") as file:
         for line in file:
@@ -25,74 +28,100 @@ def main():
                 numLines = int(initialData[1])
                 start = initialData[2]
                 end = initialData[3]
+
             else:
                 startVillage = initialData[0]
                 endVillage = initialData[1]
                 company = initialData[2]
                 type = initialData[3]
 
-                id = line
+                id = (startVillage, endVillage, company, type)
+                reverse_id = (endVillage, startVillage, company, type)
 
                 if len(G.nodes) == 0:
                     G.add_node(id)
+                    G.add_node(reverse_id)
                 else:
                     G.add_node(id)
-                    
-                    if len(keyring) > 0 and len(vertex_dict) > 0:
-                        if startVillage in keyring:
+                    G.add_node(reverse_id)
+
+
+                    if len(vertex_dict) > 0:
+                        if endVillage in vertex_dict:
+                            neighbors = vertex_dict[endVillage]
+                            for neighbor in neighbors:
+
+                                neighborStart = neighbor[0]
+                                neighborEnd = neighbor[1]
+                                neighborCompany = neighbor[2]
+                                neighborType = neighbor[3]
+
+                                if company == neighborCompany and type == neighborType: 
+                                    G.add_edge(id, neighbor, weight = 10)
+                                    print("added edge between " + id[0] + id[1] + " and " + neighbor[0] + neighbor[1] + " weight 10")
+                                elif company == neighborCompany and type != neighborType:
+                                    G.add_edge(id, neighbor, weight = 12)
+                                    print("added edge between " + id[0] + id[1] + " and " + neighbor[0] + neighbor[1] + " weight 12")
+                                elif company != neighborCompany and type == neighborType:
+                                    G.add_edge(id, neighbor, weight = 15)
+                                    print("added edge between " + id[0] + id[1] + " and " + neighbor[0] + neighbor[1] + " weight 15")
+                        if startVillage in vertex_dict:
                             neighbors = vertex_dict[startVillage]
                             for neighbor in neighbors:
-
-                                info = neighbor.split()
-                                neighborStart = info[0]
-                                neighborEnd = info[1]
-                                neighborCompany = info[2]
-                                neighborType = info[3]
-
-                                if company == neighborCompany and type == neighborType: 
-                                    G.add_edge(id, neighbor, weight = 10)
-                                elif company == neighborCompany and type != neighborType:
-                                    G.add_edge(id, neighbor, weight = 12)
-                                elif company != neighborCompany and type == neighborType:
-                                    G.add_edge(id, neighbor, weight = 15)
-
-                    if len(keyringEnd) > 0 and len(vertex_dictEnd) > 0:
-                        if endVillage in keyringEnd:
-                            neighbors = vertex_dictEnd[endVillage]
-                            for neighbor in neighbors:
                                 
-                                info = neighbor.split()
-                                neighborStart = info[0]
-                                neighborEnd = info[1]
-                                neighborCompany = info[2]
-                                neighborType = info[3]
+                                neighborStart = neighbor[0]
+                                neighborEnd = neighbor[1]
+                                neighborCompany = neighbor[2]
+                                neighborType = neighbor[3]
 
+                                
                                 if company == neighborCompany and type == neighborType: 
-                                    G.add_edge(id, neighbor, weight = 10)
+                                    G.add_edge(reverse_id, neighbor, weight = 10)
+                                    print("added reverse edge between " + reverse_id[0] + reverse_id[1] + " and " + neighbor[0] + neighbor[1] + " weight 10")
                                 elif company == neighborCompany and type != neighborType:
-                                    G.add_edge(id, neighbor, weight = 12)
+                                    G.add_edge(reverse_id, neighbor, weight = 12)
+                                    print("added reverse edge between " + reverse_id[0] + reverse_id[1] + " and " + neighbor[0] + neighbor[1] + " weight 12")
                                 elif company != neighborCompany and type == neighborType:
-                                    G.add_edge(id, neighbor, weight = 15)
+                                    G.add_edge(reverse_id, neighbor, weight = 15)
+                                    print("added reverse edge between " + reverse_id[0] + reverse_id[1] + " and " + neighbor[0] + neighbor[1] + " weight 15")
+
+
+                    
+                             
    
-                    if startVillage not in keyring:
+                    if startVillage not in vertex_dict:
                         vertex_dict[startVillage] = [id]
-                        keyring.add(startVillage)
                     else:
                         vertex_dict[startVillage].append(id)
-                        keyring.add(startVillage)
 
-                    if endVillage not in keyringEnd:
-                        vertex_dictEnd[endVillage] = [id]
-                        keyringEnd.add(endVillage)
-                    else:
-                        vertex_dictEnd[endVillage].append(id)
-                        keyringEnd.add(endVillage)
-            
+
+
             counter += 1
 
+    #potentialStarts = vertex_dict[start]
+    #potentialEnds = vertex_dict[end]
+
+    #for node in potentialStarts:
+       #G.add_edge(pseudoStart, node)
+    #for node in potentialEnds:
+        #G.add_edge(node, pseudoEnd)
+
+    #path, path_length = nx.single_source_dijkstra(G, source=pseudoStart, target=pseudoEnd)
+    #print(path)
+    #print(path_length)
+
+    printdict(vertex_dict)
 
     nx.draw(G, with_labels=True)
     plt.show()
+
+def printdict(dict):
+    for cluster in dict:
+        print(cluster + " ", end="")
+        for node in dict[cluster]:
+            print(node[0] + node[1] + " ", end="")
+        print()
+
 
 if __name__ == "__main__":
     main()
